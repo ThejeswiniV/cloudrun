4bb41d3cef7eb94b22e9b2568628ec28f51df6ed
ThejeswiniV <110930247+ThejeswiniV@users.noreply.github.com>
2023-03-30 10:31:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 # Configure this environment variable via app.yaml
 # CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
-CLOUD_STORAGE_BUCKET = 'product'
+CLOUD_STORAGE_BUCKET = 'product11'
 credentials = service_account.Credentials.from_service_account_file("credentials.json")
 
 @app.route('/')
